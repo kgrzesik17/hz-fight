c990e7bfdcac1b44a4e16a213a5f587111b502d8
Kacper <kgrzesik17@gmail.com>
2025-08-24 16:50:14 +0200

    fight simulator

--- main.py
+++ main.py
@@ -3,36 +3,66 @@ import random
 class Character:
     def __init__(self, strength, condition, brains, intuition):
         self.damage = strength
+        self.condition = condition
         self.hp = condition * 10
         self.brains = brains
         self.intuition = intuition
 
+    def reset(self):
+        self.hp = self.condition * 10
 
-player = Character(10, 10, 50, 20)
-enemy = Character(10, 10, 20, 20)
 
-
-def hit(player, enemy):   
+def hit(player, enemy, echo = False):   
     if random.randint(1, 100) > enemy.intuition:
         if random.randint(1, 100) <= player.brains:
-            return player.damage * (random.randint(16, 24) / 10)
+            # crit
+            damage = round(player.damage * (random.randint(16, 24) / 10))  # 1.6 - 2.4 spread (TODO)
+            if echo: 
+                print(f"{damage}!!")
+            return damage
         else:
-            return player.damage
+            # normal hit
+            damage = round(player.damage)  # TODO: spread check
+            if echo: 
+                print(f"{damage}") 
+            return damage
     else:
+        # dodge
+        damage = 0
+        if echo:
+            print("Unik!")
         return 0
 
 
 def fight(player, enemy):
-    while player.hp > 0 and enemy.hp > 0:
-        enemy.hp =- hit(player, enemy)
-        if enemy.hp == 0:
+    player.reset()
+    enemy.reset()
+
+    while True:
+        enemy.hp -= hit(player, enemy)
+        # print(f"Enemy HP: {enemy.hp}")
+        if enemy.hp <= 0:
             return True
         
-        player.hp =- hit(enemy, player)
-        if player.hp == 0:
+        player.hp -= hit(enemy, player)
+        # print(f"Your HP: {player.hp}")
+        if player.hp <= 0:
             return False
 
-def main(player, enemy):
-    pass
+def main():
+    player = Character(10, 10, 20, 2)
+    enemy = Character(10, 10, 20, 20)
+    echo = False  # be careful with echo when simulating multiple battles
+    simulation_count = 10000  # number of simulates to perform
 
-main(player, enemy)
+    wins = 0
+
+    for i in range(simulation_count):
+        if(fight(player, enemy)):
+            wins += 1
+
+    winrate = (wins / simulation_count) * 100
+
+    print(f"You won {wins} of {simulation_count} fights ({winrate}% winrate.).")
+
+main()
